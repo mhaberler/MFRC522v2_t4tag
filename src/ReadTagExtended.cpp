925d83bbc892a71274e39abc0308e460bcce6473
Michael Haberler <git@mah.priv.at>
2023-12-22 08:11:59 +0100

    use toJson()

--- src/ReadTagExtended.cpp
+++ src/ReadTagExtended.cpp
@@ -39,6 +39,7 @@ MFRC522Extended mfrc522{driver}; // Create MFRC522 instance.
 
 
 NfcAdapter nfc = NfcAdapter(&mfrc522);
+StaticJsonDocument<2048> jsondoc;
 
 void setup(void)
 {
@@ -55,69 +56,14 @@ void setup(void)
 
 void loop(void)
 {
-      if (nfc.tagPresent())
-      {
-            Serial.println("Reading NFC tag");
-            NfcTag tag = nfc.read();
-            Serial.println(tag.getTagType());
-            Serial.print("UID: ");
-            Serial.println(tag.getUidString());
+  if (nfc.tagPresent())
+  {
+    Serial.println("Reading NFC tag");
+    NfcTag tag = nfc.read();
 
-            if (tag.hasNdefMessage()) // every tag won't have a message
-            {
-
-                  NdefMessage message = tag.getNdefMessage();
-                  Serial.print("\nThis NFC Tag contains an NDEF Message with ");
-                  Serial.print(message.getRecordCount());
-                  Serial.print(" NDEF Record");
-                  if (message.getRecordCount() != 1)
-                  {
-                        Serial.print("s");
-                  }
-                  Serial.println(".");
-
-                  // cycle through the records, printing some info from each
-                  int recordCount = message.getRecordCount();
-                  for (int i = 0; i < recordCount; i++)
-                  {
-                        Serial.print("\nNDEF Record ");
-                        Serial.println(i + 1);
-                        NdefRecord record = message.getRecord(i);
-                        // NdefRecord record = message[i]; // alternate syntax
-
-                        Serial.print("  TNF: ");
-                        Serial.println(record.getTnf());
-                        Serial.print("  Type: ");
-                        PrintHexChar(record.getType(), record.getTypeLength()); // will be "" for TNF_EMPTY
-
-                        // The TNF and Type should be used to determine how your application processes the payload
-                        // There's no generic processing for the payload, it's returned as a byte[]
-                        int payloadLength = record.getPayloadLength();
-                        const byte *payload = record.getPayload();
-
-                        // Print the Hex and Printable Characters
-                        Serial.print("  Payload (HEX): ");
-                        PrintHexChar(payload, payloadLength);
-
-                        // Force the data into a String (might work depending on the content)
-                        // Real code should use smarter processing
-                        String payloadAsString = "";
-                        for (int c = 0; c < payloadLength; c++)
-                        {
-                              payloadAsString += (char)payload[c];
-                        }
-                        Serial.print("  Payload (as String): ");
-                        Serial.println(payloadAsString);
-
-                        // id is probably blank and will return ""
-                        if (record.getIdLength() > 0)
-                        {
-                              Serial.print("  ID: ");
-                              PrintHexChar(record.getId(), record.getIdLength());
-                        }
-                  }
-            }
-            nfc.haltTag();
-      }
-      delay(1000);
+    tag.toJson(jsondoc);
+    serializeJsonPretty(jsondoc, Serial);
+    // nfc.haltTag();
+  }
+  delay(1000);
 }
